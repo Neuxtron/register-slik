fd4ba1a97e3db408d816f1c7ab157e7bef3ca41b
Neuxtron <71965568+Neuxtron@users.noreply.github.com>
2024-01-16 15:47:44 +0700

    update navigation

--- src/pages/Tambah/index.tsx
+++ src/pages/Tambah/index.tsx
@@ -1,10 +1,16 @@
 import { FormEvent } from "react"
 import TambahInput from "./TambahInput"
 import styles from "./styles.module.scss"
+import { useNavigate } from "react-router"
 
 function TambahPage() {
+  const navigate = useNavigate()
+  
   const onTambah = (event: FormEvent) => {
     event.preventDefault()
+  }
+  const onBatal = () => {
+    navigate("/")
   }
   
   return (
@@ -16,7 +22,7 @@ function TambahPage() {
           <TambahInput label="NIK" name="nik" type="number" placeholder="Masukkan NIK..." />
           <div className={styles.actions}>
             <button>Tambah</button>
-            <button>Batal</button>
+            <button type="button" onClick={onBatal}>Batal</button>
           </div>
         </form>
       </div>
